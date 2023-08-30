845de9a9dd2892556281db171b54a9c5de5d8871
danesinoo <carlorosso02@gmail.com>
2023-08-31 01:17:10 +0200

    refusi, ho aggiunto le query al latex

--- queries/query4.sql
+++ queries/query4.sql
@@ -4,4 +4,4 @@ SELECT ordine.lista, sum(prodotto.prezzo * contenuto.quantita) as totale
 FROM ordine, prodotto, contenuto
 WHERE ordine.lista = contenuto.lista
 AND prodotto.id = contenuto.prodotto
-GROUP BY ordine.lista) as costo_ordini
+GROUP BY ordine.lista
