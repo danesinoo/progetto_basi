d1d152ee4650a022d26c66c5728dc0c4a43fa682
Carlo :) <82663208+danesinoo@users.noreply.github.com>
2023-08-30 22:15:52 +0200

    Update query4.sql

--- queries/query4.sql
+++ queries/query4.sql
@@ -1,9 +1,7 @@
 -- numero ordine e costo totale
 
-SELECT lista.id, totale
-from lista, (select ordine.lista, sum(prodotto.prezzo * contenuto.quantita) as totale
+SELECT ordine.lista, sum(prodotto.prezzo * contenuto.quantita) as totale
 FROM ordine, prodotto, contenuto
 WHERE ordine.lista = contenuto.lista
 AND prodotto.id = contenuto.prodotto
 GROUP BY ordine.lista) as costo_ordini
-WHERE lista.id = lista
