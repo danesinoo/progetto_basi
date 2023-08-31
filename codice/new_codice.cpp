16c1344f71da32c44595435aecf0eabea04277b7
bassupreme <leonardo.basso.7@studenti.unipd.it>
2023-08-31 11:06:41 +0200

    Aggiunte all'interno del file c++ le queries implementate da carlo.

--- codice/new_codice.cpp
+++ codice/new_codice.cpp
@@ -91,17 +91,63 @@ void query_3(PGconn* conn) {
 	print_results(res);
 }
 
+void query_4(PGconn* conn) {
+	PGresult* res;
+	std::string query = "SELECT ordine.lista, sum(prodotto.prezzo * contenuto.quantita) as totale FROM ordine, prodotto, contenuto WHERE ordine.lista = contenuto.lista AND prodotto.id = contenuto.prodotto GROUP BY ordine.lista";
+		res = PQexec(conn, query.c_str());
+	print_results(res);
+}
+
+void query_5(PGconn* conn) {
+	PGresult* res;
+	std::string query = "SELECT lista.nome, totale FROM (SELECT tab.id, sum(prezzo * quantita) as totale FROM (SELECT lista.id as id, prodotto.prezzo, contenuto.quantita FROM lista, prodotto, contenuto WHERE lista.id = contenuto.lista AND prodotto.id = contenuto.prodotto) as tab LEFT JOIN ordine ON tab.id = lista WHERE indirizzo is NULL GROUP BY tab.id) as tab2, lista WHERE tab2.id = lista.id";
+	res = PQexec(conn, query.c_str());
+	print_results(res);
+}
+
+void query_6(PGconn* conn) {
+	PGresult* res;
+	std::string query = "SELECT DATE(data_creazione) AS day, sum(totale) FROM lista, (SELECT ordine.lista, sum(prodotto.prezzo * contenuto.quantita) AS totale FROM ordine, prodotto, contenuto WHERE ordine.lista = contenuto.lista AND prodotto.id = contenuto.prodotto GROUP BY ordine.lista) as costo_ordini WHERE lista.id = lista GROUP BY day;";
+	res = PQexec(conn, query.c_str());
+	print_results(res);
+}
+
+void query_7(PGconn* conn) {
+	PGresult* res;
+	std::string query = "SELECT EXTRACT(YEAR from data_creazione) AS year, EXTRACT(MONTH from data_creazione) AS month, sum(totale) FROM lista, (SELECT ordine.lista, sum(prodotto.prezzo * contenuto.quantita) AS totale FROM ordine, prodotto, contenuto WHERE ordine.lista = contenuto.lista AND prodotto.id = contenuto.prodotto GROUP BY ordine.lista) AS costo_ordini WHERE lista.id = lista GROUP BY year, month ORDER BY year, month;";
+	res = PQexec(conn, query.c_str());
+	print_results(res);
+}
+
+void query_8(PGconn* conn) {
+	PGresult* res;
+	std::string query = "SELECT p.tag, sum(p.prezzo * c.quantita) as totale FROM prodotto as p, contenuto as c, ordine as o WHERE p.id = c.prodotto AND o.lista = c.lista GROUP BY p.tag HAVING sum(p.prezzo * c.quantita) > 100 ORDER BY totale DESC;";
+	res = PQexec(conn, query.c_str());
+	print_results(res);
+}
+
+
 int main() {
 	std::map<unsigned int, std::string> messaggi;
 	messaggi[0] = "0: esci";
 	messaggi[1] = "1: numero prodotti comprati da quali utenti in un certo periodo";
 	messaggi[2] = "2: numero di ordini effettuati da quali utenti";
 	messaggi[3] = "3: utenti e numero di carrelli da loro creati che non sono poi stati processati come ordini.";
+	messaggi[4] = "4: numero ordine e costo totale";
+	messaggi[5] = "5: nome carrello e costo totale";
+	messaggi[6] = "6: guadagno totale giornaliero";
+	messaggi[7] = "7: guadagno totale mensile";
+	messaggi[8] = "8: guadagno totale in base alla categoria di prodotti venduti";
 
 	std::map<unsigned int, void (*)(PGconn*)> queries;
 	queries[1] = &query_1;
 	queries[2] = &query_2;
 	queries[3] = &query_3;
+	queries[4] = &query_4;
+	queries[5] = &query_5;
+	queries[6] = &query_6;
+	queries[7] = &query_7;
+	queries[8] = &query_8;
 	
 	// SETUP CONNESSIONE al DB
 	PGconn* conn;
